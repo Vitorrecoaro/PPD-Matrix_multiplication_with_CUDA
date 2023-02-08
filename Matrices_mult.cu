065a21c42e1cd3cfacb33a48518dcb52a11670a9
Vitor Recoaro <vitordearec@gmail.com>
2023-02-08 16:58:06 -0300

    add: Run multiplication of matrices with CUDA

--- Matrices_mult.cu
+++ Matrices_mult.cu
@@ -2,33 +2,121 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
+#include <string.h>
+#include <math.h>
 
-// Generate Matrix with float values between [0...1000]
-void generateRandMatrix(float *matrix, int nElem){
-    int i;
-    int tam = nElem * nElem;
-    unsigned int seed = time(NULL);
+void generateRandMatrix(double *matrix, int nElem);
+void showMatrix(double *matrix, int nElem);
+void multMatricesSeq(double *matrixA, double *matrixB, double *matrixC, int nElem);
+__global__ void multMatricesGPU(double *matrixA, double *matrixB, double *matrixC, int nElem);
+double averageError(double *matrixA, double *matrixB, int nElem);
 
-    #pragma omp parallel for
-    for (i = 0; i < tam; i++){
-        matrix[i] = ((float) rand_r(&seed) / (float) RAND_MAX) * 1000;
+int main(int argv, char *argc[]){
+    int nElem = 5;
+    int runSeq = 0;
+    int runCUDA = 0;
+    double *d_A, *d_B, *d_C, *h_A, *h_B, *h_C, *C_Cuda;
+    dim3 block, grid;
+
+    switch(argv){
+        case(2):
+            nElem = atoi(argc[1]);
+            break;
+        case(3):
+            nElem = atoi(argc[1]);
+            if (argc[2][0] == '-'){
+                if (strchr(argc[2], 'C') != NULL){
+                    runCUDA = 1;
+                }
+                if (strchr(argc[2], 'S') != NULL){
+                    runSeq = 1;
+                }
+            }
+            else{
+                printf("You can run with this flags:\n");
+                printf("    -S: To run sequential method.\n");
+                printf("    -C: To run with cuda paralelization.\n");
+            }
+            break;
     }
+
+    block.x = 32;
+    block.y = 32;
+    block.z = 1;
+
+    grid.x = (nElem + block.x - 1)/block.x;
+    grid.y = (nElem + block.y - 1)/block.y;
+    grid.z = 1;
+
+    cudaMalloc(&d_A, nElem * nElem * sizeof(double));
+    cudaMalloc(&d_B, nElem * nElem * sizeof(double));
+    cudaMalloc(&d_C, nElem * nElem * sizeof(double));
+    h_A = (double*) malloc(nElem * nElem * sizeof(double));
+    h_B = (double*) malloc(nElem * nElem * sizeof(double));
+    h_C = (double*) malloc(nElem * nElem * sizeof(double));
+    C_Cuda = (double*) malloc(nElem * nElem * sizeof(double));
+
+    generateRandMatrix(h_A, nElem);
+    generateRandMatrix(h_B, nElem);
+    cudaMemcpy(d_A, h_A, nElem * nElem * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, nElem * nElem * sizeof(double), cudaMemcpyHostToDevice);
+
+    if (runCUDA){
+        multMatricesGPU<<< grid, block >>>(d_A, d_B, d_C, nElem);
+        cudaMemcpy(C_Cuda, d_C, nElem * nElem * sizeof(double), cudaMemcpyDeviceToHost);
+    }
+
+    if (runSeq){
+        multMatricesSeq(h_A, h_B, h_C, nElem);
+    }
+
+    if (runCUDA && runSeq){
+        printf("Average error: %f\n", averageError(C_Cuda, h_C, nElem));
+        showMatrix(h_C, nElem);
+        showMatrix(C_Cuda, nElem);
+    }
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    free(C_Cuda);
+    
 }
 
-// Calculate a multiplication of matrices on sequential form.
-void multMatricesSeq(float *matrixA, float *matrixB, float *matrixC, int nElem){
+// Calculate the multiplication of matrices on sequential form.
+void multMatricesSeq(double *matrixA, double *matrixB, double *matrixC, int nElem){
     int i, j, k;
 
     for (i = 0; i < nElem; i++){
-        for (j = 0; j < nElem; j++){
-            for (k = 0; k < nElem; k++){
+        for (k = 0; k < nElem; k++){
+            matrixC[i * nElem + j] = 0.0;
+            for (j = 0; j < nElem; j++){
                 matrixC[i * nElem + j] += matrixA[i * nElem + k] * matrixB[k * nElem + j];
             }
         }
     }
 }
 
-void showMatrix(float *matrix, int nElem){
+// Calculate the multiplication of matrices with CUDA support.
+__global__ void multMatricesGPU(double *matrixA, double *matrixB, double *matrixC, int nElem){
+    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int i;
+    double sum = 0;
+
+    if(x < nElem && y < nElem){
+        for (i = 0; i < nElem; i++){
+            sum += matrixA[x * nElem + i] * matrixB[i * nElem + y];
+        }
+    }
+
+    matrixC[x * nElem + y] = sum;
+}
+
+void showMatrix(double *matrix, int nElem){
     int i;
 
     for (i = 0; i < nElem * nElem; i++){
@@ -38,24 +126,28 @@ void showMatrix(float *matrix, int nElem){
     printf("\n");
 }
 
-int main(){
-    int nElem = 2;
-    float *A, *B, *C;
+// Generate Matrix with double values between [0...1000]
+void generateRandMatrix(double *matrix, int nElem){
+    int i;
+    int tam = nElem * nElem;
+    unsigned int seed = time(NULL);
 
-    cudaMallocManaged(&A, nElem * nElem * sizeof(float));
-    cudaMallocManaged(&B, nElem * nElem * sizeof(float));
-    cudaMallocManaged(&C, nElem * nElem * sizeof(float));
+    #pragma omp parallel for
+    for (i = 0; i < tam; i++){
+        matrix[i] = ((double) rand_r(&seed) / (double) RAND_MAX) * 1000;
+    }
+}
 
-    generateRandMatrix(A, nElem);
-    generateRandMatrix(B, nElem);
-
-    multMatricesSeq(A, B, C, nElem);
-    showMatrix(A, nElem);
-    showMatrix(B, nElem);
-    showMatrix(C, nElem);
-
-    cudaFree(A);
-    cudaFree(B);
-    cudaFree(C);
+double averageError(double *matrixA, double *matrixB, int nElem){
+    double errorAvg = 0;
+    int i;
     
+    #pragma omp parallel for reduction(+:errorAvg)
+    for (i = 0; i < nElem * nElem; i++){
+        if (matrixA[i] != matrixB[i]){
+            errorAvg += abs(matrixA[i] - matrixB[i]);
+        }
+    }
+
+    return (errorAvg/(nElem * nElem));
 }
